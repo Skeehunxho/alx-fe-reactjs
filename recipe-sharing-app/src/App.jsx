dc85b516a90a46de85f8372658da8f34cf11b7d8
Skeehunxho <Kentmmmuia18@gmail.com>
2024-08-23 14:55:31 +0300

    Update changes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
+import DeleteRecipeButton from './components/DeleteRecipeButton';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
               <div>
                 <AddRecipeForm />
                 <RecipeList />
+                <DeleteRecipeButton />
               </div>
             }
           />
