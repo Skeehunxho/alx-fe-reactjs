8abd92c6d6eaa4583d40dccfd5e32a68af7ff10a
Skeehunxho <Kentmmmuia18@gmail.com>
2024-08-23 14:53:13 +0300

    Modify Changes

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,20 @@
-// RecipeList component
-import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from '../recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
+  const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
-      {recipes.map(recipe => (
+      {recipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
+          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
         </div>
       ))}
     </div>
   );
-  
 };
-export default RecipeList
+
+export default RecipeList;
