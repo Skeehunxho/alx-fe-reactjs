7acf1687dc7917f09700cf22d90b9b48ec50d882
Skeehunxho <Kentmmmuia18@gmail.com>
2024-09-03 14:34:01 +0300

    Update Changes

---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,14 @@
+// src/pages/ProfileDetails.jsx
+function ProfileDetails() {
+    return (
+      <div>
+        <h2>Profile Details</h2>
+        <p>Name: John Doe</p>
+        <p>Email: johndoe@example.com</p>
+        <p>Joined: January 1, 2020</p>
+      </div>
+    );
+  }
+  
+  export default ProfileDetails;
+  
---
+++ react-router-advanced/src/components/ProfileSetting.jsx
@@ -0,0 +1,24 @@
+// src/pages/ProfileSettings.jsx
+function ProfileSettings() {
+    return (
+      <div>
+        <h2>Profile Settings</h2>
+        <form>
+          <label>
+            Change Email:
+            <input type="email" placeholder="Enter new email" />
+          </label>
+          <br />
+          <label>
+            Change Password:
+            <input type="password" placeholder="Enter new password" />
+          </label>
+          <br />
+          <button type="submit">Update</button>
+        </form>
+      </div>
+    );
+  }
+  
+  export default ProfileSettings;
+  
